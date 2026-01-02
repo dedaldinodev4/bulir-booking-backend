34bb1e6a930081814ed5a69e1efc8537c6f4ae55
Dedaldino Daniel <dedaldinodev4@gmail.com>
2026-01-02 19:32:47 +0100

    feat: add env creation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 
 import { server } from './server'
+import { env } from './env';
 
-
-server.listen(3000, () => {
-  console.log(`Server running on port 3000.`)
+server.listen(env.PORT, () => {
+  console.log(`Server running on port ${env.PORT}`)
 });
 
 server.get("/", (request, response) => {
