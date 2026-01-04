e7db77748352473a6a36f900d9a3ce7865d13ff6
Dedaldino Daniel <dedaldinodev4@gmail.com>
2026-01-04 12:31:19 +0100

    refactor: add authorizantion on services routes

--- src/usecases/Service/routes/index.ts
+++ src/usecases/Service/routes/index.ts
@@ -7,18 +7,32 @@ import { deleteServiceFactory } from "../DeleteService/DeleteServiceFactory";
 import { createServiceFactory } from '../CreateService/CreateServiceFactory'
 import { findByProviderServiceFactory } from "../FindByProviderService/FindByProviderServiceFactory";
 
+import { ensuredAuthenticated } from "../../../middlewares/ensuredAuthenticated";
+import { is } from "../../../middlewares/authorization";
+
 export const serviceRoutes = Router();
 
 serviceRoutes.route('/')
-  .post((request, response) => { return createServiceFactory().handle(request, response) } )
-  .get((request, response) => { return findAllServicesFactory().handle(request, response) } )
+  .post(
+    ensuredAuthenticated(),
+    is('PROVIDER', 'ADMIN'),
+    (request, response) => { return createServiceFactory().handle(request, response) } )
+  .get(
+    ensuredAuthenticated(),
+    (request, response) => { return findAllServicesFactory().handle(request, response) } )
 
 serviceRoutes.route('/:id')
-  .get((request, response) => { return findByIdServiceFactory().handle(request, response) } )
-  .put((request, response) => { return updateServiceFactory().handle(request, response) } )
-  
-serviceRoutes.route('/:id/deletedBy/:user')
-  .delete((request, response) => { return deleteServiceFactory().handle(request, response) } )
+  .get(
+    ensuredAuthenticated(),
+    (request, response) => { return findByIdServiceFactory().handle(request, response) } )
+  .put(
+    ensuredAuthenticated(),
+    (request, response) => { return updateServiceFactory().handle(request, response) } )
+  .delete(
+    ensuredAuthenticated(),
+    (request, response) => { return deleteServiceFactory().handle(request, response) } )
 
 serviceRoutes.route('/byProvider/:providerId')
-  .get((request, response) => { return findByProviderServiceFactory().handle(request, response) } )
+  .get(
+    ensuredAuthenticated(),
+    (request, response) => { return findByProviderServiceFactory().handle(request, response) } )
