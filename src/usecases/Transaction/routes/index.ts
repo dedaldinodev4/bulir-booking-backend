bb98e0b3854b70494fa03d0571c258e0464c370e
Dedaldino Daniel <dedaldinodev4@gmail.com>
2026-01-04 13:18:20 +0100

    refactor: add authorizantion on transactions routes

--- src/usecases/Transaction/routes/index.ts
+++ src/usecases/Transaction/routes/index.ts
@@ -6,15 +6,29 @@ import { updateTransactionFactory } from "../UpdateTransaction/UpdateTransaction
 import { deleteTransactionFactory } from "../DeleteTransaction/DeleteTransactionFactory";
 import { createTransactionFactory } from '../CreateTransaction/CreateTransactionFactory'
 
+import { ensuredAuthenticated } from "../../../middlewares/ensuredAuthenticated";
+import { is } from "../../../middlewares/authorization";
+
 export const transactionRoutes = Router();
 
 transactionRoutes.route('/')
-  .post((request, response) => { return createTransactionFactory().handle(request, response) } )
-  .get((request, response) => { return findAllTransactionsFactory().handle(request, response) } )
+  .post(
+    ensuredAuthenticated(),
+    is('PROVIDER', 'CLIENT'),
+    (request, response) => { return createTransactionFactory().handle(request, response) })
+  .get(
+    ensuredAuthenticated(),
+    (request, response) => { return findAllTransactionsFactory().handle(request, response) })
 
 transactionRoutes.route('/:id')
-  .get((request, response) => { return findByIdTransactionFactory().handle(request, response) } )
-  .put((request, response) => { return updateTransactionFactory().handle(request, response) } )
-  
-transactionRoutes.route('/:id/deletedBy/:user')
-  .delete((request, response) => { return deleteTransactionFactory().handle(request, response) } )
+  .get(
+    ensuredAuthenticated(),
+    (request, response) => { return findByIdTransactionFactory().handle(request, response) })
+  .put(
+    ensuredAuthenticated(),
+    (request, response) => { return updateTransactionFactory().handle(request, response) })
+  .delete(
+    ensuredAuthenticated(),
+    is('ADMIN'),
+    (request, response) => { return deleteTransactionFactory().handle(request, response) })
+
