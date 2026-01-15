675467949ad99f2dd3b6b6898214ae8a7184f90b
Dedaldino Daniel <dedaldinodev4@gmail.com>
2026-01-16 00:27:02 +0100

    refactor: improve findAll transactions usecase

--- src/usecases/Transaction/FindAllTransactions/FindAllTransactionsController.ts
+++ src/usecases/Transaction/FindAllTransactions/FindAllTransactionsController.ts
@@ -12,13 +12,14 @@ export class FindAllTransactionsController {
     const page = parseInt(request.query.page as string);
     const limit = parseInt(request.query.limit as string);
     const order = request.query.order as 'asc' | 'desc';
+    const orderBy = request.query.orderBy as string;
     const bookingId = (request.query.bookingId as string);
     const walletId = (request.query.walletId as string);
     const type = (request.query.type as TransactionType);
     const status = (request.query.status as TransactionStatus)
     try {
       const data = await this.findAllTransactionUseCase.execute({
-        page, limit, order, bookingId, walletId, type, status
+        page, limit, order, bookingId, walletId, type, status, orderBy
       })
       return response.status(200).json(data);
 
